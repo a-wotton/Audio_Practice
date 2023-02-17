010b81f03a853bbd9a2e2a59f7b00f85de0d48d5
a-wotton <a_wotton@fanshaweonline.ca>
2023-02-17 10:25:16 -0500

    added js functionality

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ let albumCovers = document.querySelectorAll(".audio img")
 
 function playTrack() {
     debugger;
+    let currentAudio = document.querySelector(`audio[data-trackref="${this.dataset.trackref}"]`);
+    currentAudio.play();
 }
 
 albumCovers.forEach(album => album.addEventListener("click", playTrack))
